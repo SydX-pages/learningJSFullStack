c232d78c50a66e11bdb081dadb6c98c227030ef6
SydX-pages <xusyd1222@gmail.com>
2026-06-11 14:50:25 +0800

    part3 finished

--- part3/a/index.js
+++ part3/a/index.js
@@ -14,21 +14,20 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((err) => next(err));
 });
 
 app.get("/api/notes/:id", (request, response, next) => {
